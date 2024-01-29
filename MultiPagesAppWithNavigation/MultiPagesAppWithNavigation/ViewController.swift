8354727bec6ff74f4a62103092be4c6ce59b4355
Francisco Ricardo Andraschko <124284004+fricardo-it@users.noreply.github.com>
2024-01-28 22:59:23 -0500

    Add files via upload

---
+++ MultiPagesAppWithNavigation/MultiPagesAppWithNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiPagesAppWithNavigation
+//
+//  Created by Francisco Ricardo Andraschko on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
